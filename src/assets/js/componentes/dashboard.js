a8f9a352137531cb9d7aa7daaa7ee25f06506ad6
stephHiyagon <mv.hiyagon@gmail.com>
2017-08-02 13:50:49 -0500

    navbar y dashboard

---
+++ src/assets/js/componentes/dashboard.js
@@ -0,0 +1,22 @@
+const Dashboard =(update)=>{
+  const div=$('<div class="container"></div>');
+  const row=$('<div class="row"></div>');
+  const col4=$('<div class="col s12 col m4 center-align"></div>');
+  const photo=$('<img src="" alt="foto de perfil"/>');
+  const name=$('<p class="blue-text">Juan Perez</p>');
+  const col8=$('<div class="col s12 col m8"></div>');
+  const title=$('<div><h4>News</h4></div>');
+  var noticias= (state.news).forEach((news)=>{
+        news.attachments.data[0].title;
+        console.log(news.attachments.data[0].title);
+        title.append('<h5>'+news.attachments.data[0].title+'</h5>')
+  });
+  div.append(row);
+  row.append(col4);
+  col4.append(photo);
+  col4.append(name);
+  row.append(col8);
+  col8.append(title);
+
+  return div;
+}
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,58 +3,36 @@ const render = (root) => {
   const wrapper = $('<div class="wrapper"></div>');
   const update = function() {
     render(root);
-  }
+  };
 
-
-  if(state.modal==null){
-    wrapper.append(Header(update));
-    wrapper.append(Board(update));
-  }
-
-  if(state.modal=="modal"){
-    wrapper.append(Modal(update));
-  }
-
-  // if(state.modal)
-;
+  wrapper.append(Navbar());
+  wrapper.append(Dashboard(update));
   root.append(wrapper);
 
 }
 
 
 const state = {
-  pin:null,
-  page:null,
-  pin2:null,
-  page2:null,
-  pines:null,
-  modal:null,
-  id:null,
-  data:null,
-  image:null,
-  creator:null,
-  nameBoard:null,
-  titulo:null,
-  nota:null,
-  publicado:null,
-  site:null
+  user:null,
+  news:null,
+  page:null
 }
 
 
 $(_=>{
 
-  $.get('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AZ2i9KfK4bcYVlQFmQ2ZjB8JS7DRFM4qVqQ9s7REItsdIWA_dQAAAAA&fields=id%2Clink%2Cnote%2Curl%2Ccreated_at%2Ccounts%2Ccolor%2Cboard%2Cattribution%2Cmedia%2Cmetadata%2Coriginal_link%2Ccreator%2Cimage', (data) => {
+  $.get('https://hackathon-ef798.firebaseio.com/news.json', (data) => {
 
       if (!data) { return alert('no hay data gg');}
 
-      state.pin = data.data;
-      state.page= data.page;
+      // state.news = data.data[0].attachments.data[0].title;
+      state.news=data.data;
 
-      console.log(data.data);
+      console.log(state.news);
 
       const root = $('#root');
       render(root);
-
+      $(".button-collapse").sideNav();
     });
 
 
