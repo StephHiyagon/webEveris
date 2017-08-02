3937472586a163cc63ae929a5c532ee1cb57db10
Betsi Loayza <betsi18_12@hotmail.com>
2017-08-02 12:37:14 -0500

    login

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,56 +1,31 @@
 const render = (root) => {
   root.empty();
   const wrapper = $('<div class="wrapper"></div>');
-  const update = function() {
-    render(root);
+  if (state.page==0) {
+    wrapper.append(Login(_=>render(root)));
+    root.append(wrapper);
+  }else if (state.page==1) {
+    wrapper.append(Dashboard());
+    root.append(wrapper);
   }
-
-
-  if(state.modal==null){
-    wrapper.append(Header(update));
-    wrapper.append(Board(update));
-  }
-
-  if(state.modal=="modal"){
-    wrapper.append(Modal(update));
-  }
-
-  // if(state.modal)
-;
-  root.append(wrapper);
 
 }
 
-
 const state = {
-  pin:null,
-  page:null,
-  pin2:null,
-  page2:null,
-  pines:null,
-  modal:null,
-  id:null,
-  data:null,
-  image:null,
-  creator:null,
-  nameBoard:null,
-  titulo:null,
-  nota:null,
-  publicado:null,
-  site:null
+  page:0,
+  user:null,
+  news:null
 }
 
 
 $(_=>{
 
-  $.get('https://api.pinterest.com/v1/boards/arabelyuska/web-ui/pins/?access_token=AZ2i9KfK4bcYVlQFmQ2ZjB8JS7DRFM4qVqQ9s7REItsdIWA_dQAAAAA&fields=id%2Clink%2Cnote%2Curl%2Ccreated_at%2Ccounts%2Ccolor%2Cboard%2Cattribution%2Cmedia%2Cmetadata%2Coriginal_link%2Ccreator%2Cimage', (data) => {
+  $.get('https://hackathon-ef798.firebaseio.com/getemployee.json', (data) => {
 
       if (!data) { return alert('no hay data gg');}
 
-      state.pin = data.data;
-      state.page= data.page;
-
-      console.log(data.data);
+      state.user = data.persona;
+      console.log(data.persona);
 
       const root = $('#root');
       render(root);
