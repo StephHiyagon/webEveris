d739c5dc06e7c95c5ba735ada1ffde3fb44a1290
Betsi Loayza <betsi18_12@hotmail.com>
2017-08-03 08:16:59 -0500

    modal1

--- src/assets/js/componentes/beneficios.js
+++ src/assets/js/componentes/beneficios.js
@@ -16,9 +16,8 @@ const Beneficios = ()=>{
   categorias.append(listaCategoria);
 
   for (var i = 0; i < 10; i++) {
-  const promocion = $('<div class="col s12 m6"></div>');
+  const promocion = $('<div class="col s12 m6 modal-trigger" id="'+state.benefits[i].id+'""><a href="#modal1"></a></div>');
   const contenedor = $('<div class="card horizontal height"></div>');
-
   const divImagen1 = $('<div class="img-promocion valign-wrapper card-image"></div>');
   const imagen = $('<img src="'+state.benefits[i].imgCompany+'">');
   const divDescrip = $('<div class="card-stacked"></div>');
@@ -34,9 +33,28 @@ const Beneficios = ()=>{
   divDescrip.append(divDescrip1);
   divDescrip1.append(divDescrip1, nombreCompania, descrip, span);
 
+
+
 }
-
-
+div.append(detalleBeneficios());
 
   return div;
+}
+
+const detalleBeneficios = () =>{
+  const container = $('<div id="modal1" class="modal"></div>');
+  const close = $('<a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">X</a>');
+  const modal = $('<div class="modal-content"></div>');
+  const colDescrip = $('<div class="col s12 m6">ljlihugftdsegfghjjkjlk</div>');
+  const colMapa = $('<div class="col s12 m6">kjhgfdsdfcgvhbjnkm</div>');
+  const mapa = $('<div id="mapa"></div>');
+  container.append(close, modal);
+  modal.append(colDescrip, colMapa);
+  colMapa.append(mapa);
+
+  $(document).ready(function(){
+   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+   $('.modal').modal();
+  });
+return container;
 }
