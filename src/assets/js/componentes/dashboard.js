15f790df2c19da5c08a6d931dbe5dc1d3e1a5fe6
stephHiyagon <mv.hiyagon@gmail.com>
2017-08-03 13:27:21 -0500

    diseño y felicitaciones

--- src/assets/js/componentes/dashboard.js
+++ src/assets/js/componentes/dashboard.js
@@ -1,15 +1,43 @@
 const Dashboard =(update)=>{
-  const div=$('<div class="container"></div>');
+  const fecha = state.user.fechaIngreso.split('-');
+  console.log(fecha);
+  var mes, tiempo;
+  console.log(fecha[1]);
+  switch (fecha[1]) {
+    case '01':
+      mes='enero'
+      break;
+    case '02':
+      mes='febrero'
+      break;
+    case '03':
+      mes='marzo'
+      break;
+    case '08':
+      mes='agosto'
+      break;
+  }
+  var hoy = new Date();
+  var diaA = hoy.getDate();
+  var mesA = hoy.getMonth()+1; //hoy es 0!
+  var anioA = hoy.getFullYear()
+  console.log(anioA);
+  if(fecha[0]<anioA){
+    tiempo=anioA - fecha[0];
+    console.log(tiempo);
+  }
+  const div=$('<div class="container-fluid"></div>');
   const row=$('<div class="row"></div>');
+  const div2=$(`<div class="col s12 felicidad valign-wrapper"><h3 class="center-align white-text">Feliz Aniversario!!!</h3><h4 class="center-align white-text">Un ${fecha[2]} de ${mes} de ${fecha[0]} uniste tu camino al nuestro, hoy cumplimos ${tiempo} año juntos y por tu compromiso te mereces este ascenso</h4></div>`);
   const col8=$('<div class="board"></div>');
-  const news=$('<h4 class="news">News</h4>');
+  const news=$('<h4 class="news col s12 center-align">Noticias</h4>');
   var noticias= (state.news).forEach((news)=>{
         news.attachments.data[0].title;
-        console.log(news.attachments.data[0].title);
+        // console.log(news.attachments.data[0].title);
 
-        const panel=$('<div class="card-panel hoverable item__stack"></div>');
+        const panel=$('<div class="item__stack"></div>');
         const anchor=$('<a href="'+news.shareURL+'"></a>')
-        const title=$('<div class="item"></div>');
+        const title=$('<div class="item black-text"></div>');
         if(news.description!=undefined && news.imgURL!=undefined){
         anchor.append(title);
         title.append(panel);
@@ -22,6 +50,7 @@ const Dashboard =(update)=>{
 
   });
   div.append(row);
+  row.append(div2);
   row.append(news)
   row.append(col8);
 
