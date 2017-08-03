9e0c7cbb8b617967929419c0275aa452a5df600c
Ana Durand <ana.dc1004@gmail.com>
2017-08-03 00:05:24 -0500

    estilo buscar

--- src/assets/js/componentes/buscar.js
+++ src/assets/js/componentes/buscar.js
@@ -2,20 +2,25 @@
 
 const Buscar = (update) => {
 
-  const parent = $('<div class="row"></div>');
-  const form = $('<form class="col s12 m8 l6 offset-l3 offset-m2 autocomplete="off""></form>');
+  const parent = $('<div class=""></div>');
+  const divForm = $('<div class="row"></div>');
+  const form = $('<form class="col s12 m8 l6 offset-l3 offset-m2" autocomplete="off"></form>');
   const row = $('<div class="row"></div>');
   const div = $('<div class="input-field col s12"><i class="material-icons prefix">search</i></div>');
-  const input = $('<input id="icon_prefix" type="text" class="validate">');
+  const input = $('<input id="icon_prefix" type="text" class="search">');
   const label = $('<label for="icon_prefix">Buscar</label>');
-  const autocomplete = $('<div class="autocomplete"></div>');
+  const autocomplete = $('<div class="autocompleted"></div>');
+  const box = $('<div class="resultado row"></div>');
+  const userDiv = $('<div class="col s12 m8 l6 offset-l3 offset-m2"></div>');
 
   div.append(input);
   div.append(label);
   div.append(autocomplete);
   row.append(div);
   form.append(row);
-  parent.append(form);
+  divForm.append(form);
+  parent.append(divForm);
+  box.append(userDiv);
 
   let filterbyKey;
 
@@ -23,7 +28,7 @@ const Buscar = (update) => {
     keyup: function(e){
               if($(e.currentTarget).val() != ""){
                  filterbyKey = filtroContacto($(this).val().toLowerCase(), autocomplete);
-                 console.log(state.personal);
+                 reRender(filterbyKey, autocomplete, userDiv);
               }else{
                 autocomplete.hide();
                 autocomplete.empty();
@@ -32,7 +37,7 @@ const Buscar = (update) => {
         }
   });
 
-
+  parent.append(box);
   return parent;
 
 }
--- src/assets/js/filter.js
+++ src/assets/js/filter.js
@@ -6,10 +6,38 @@ const filtroContacto = (query, container)=> {
   });
 }
 
-const reRender = (filtro, container) => {
+const reRender = (filtro, container, divResult) => {
     container.empty();
-    state.personal.forEach((item) => {
+    filtro.forEach((item) => {
+      container.append(showItem(item, container, divResult));
+    });
+}
 
-    container.append(placeItem(item, container));
+const showItem = (item, container, divResult) => {
+  const itemDiv = $('<div class="row"></div>');
+  const p1 = $('<span class="item-place__title col s9">'+ item.Nombre +'   '+item.Apellido +'   '+item.Apellido2 +'</span>');
+  const p2 = $('<span class="col s3">'+item.DatosEmpleado.EMPLOYEE_NUMBER+'</span>');
+  const parent = $('<div></div>');
+  const name = $('<h5>'+ item.Nombre + '  '+ item.Apellido +'</h5> <br>');
+  const mail = $('<p> '+item.DatosEmpleado.EMAIL+'</p>');
+  const codigo = $('<p> '+item.DatosEmpleado.EMPLOYEE_NUMBER+'</p>');
+  // const mail = $('<p> '+item.DatosEmpleado.EMAIL+'</p>');
+  // const mail = $('<p> '+item.DatosEmpleado.EMAIL+'</p>');
+
+  itemDiv.append(p1);
+  itemDiv.append(p2);
+
+  parent.append(name);
+  parent.append(mail);
+  parent.append(codigo);
+
+  itemDiv.on("click", (e) => {
+    e.preventDefault();
+    container.empty();
+    container.hide();
+    divResult.empty();
+    divResult.append(parent);
   });
+  container.show();
+  return itemDiv;
 }
