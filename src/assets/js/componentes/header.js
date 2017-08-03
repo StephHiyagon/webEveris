22c2c222642722a4a93354ecc950edb674ce673b
flor <condorihumanif@gmail.com>
2017-08-03 04:53:05 -0500

    comunicados en card

--- src/assets/js/componentes/header.js
+++ src/assets/js/componentes/header.js
@@ -1,22 +1,24 @@
 const Login = (update) => {
- const container = $('<div class="flex row"></div>');
- const form = $('<form class="col s6 offset-s3 flex"></form>');
- const divUser = $('<div class=" input-field"><div>');
-  const inputUser = $('<input id="last_name" type="text" class="validate">');
-  const labelUser = $('<label for="last_name">Usuario</label>');
-const divPassword = $('<div class=" input-field"><div>');
- const inputPassword = $('<input id="password" type="password" class="validate">');
- const labelPassword = $('<label for="password">Contraseña</label>');
-const button = $('<a class="center-align waves-effect waves-light btn-large">Entrar</a>');
- container.append(form);
- form.append(divUser, divPassword);
- divUser.append(inputUser, labelUser);
- divPassword.append(inputPassword, labelPassword);
- form.append(button);
+	const container = $('<div class="flex row"></div>');
+	const form = $('<form class="col s6 offset-s3 flex"></form>');
+	const divUser = $('<div class=" input-field"><div>');
+	const inputUser = $('<input id="last_name" type="text" class="validate">');
+	const labelUser = $('<label for="last_name">Usuario</label>');
+	const divPassword = $('<div class=" input-field"><div>');
+	const inputPassword = $('<input id="password" type="password" class="validate">');
+	const labelPassword = $('<label for="password">Contraseña</label>');
+	const button = $('<a class="center-align waves-effect waves-light btn-large">Entrar</a>');
 
-button.on('click', function(){
-  state.page= Notifications;
-  update();
-});
- return container;
-};
+	container.append(form);
+	form.append(divUser, divPassword);
+	divUser.append(inputUser, labelUser);
+	divPassword.append(inputPassword, labelPassword);
+	form.append(button);
+
+	button.on('click', function(){
+		state.page= Notifications;
+		update();
+	});
+	
+	return container;
+	};
--- src/assets/js/componentes/notifications.js
+++ src/assets/js/componentes/notifications.js
@@ -9,24 +9,24 @@ const Notifications = ()=>{
 		const array = json.data;
 		console.log(array);
 
-		const col2 = $("<div class='col s12 col l6 col xl6'></div>");
+		const col1 = $("<div class='col s12'></div>");
+		const sub1 = $("<div class='col s12 col l9'></div>");
+		const sub2 = $("<div class='col s12 col l3'></div>");
+		const col2 = $("<div class='col s12'></div>");
 		
-		array.forEach((obj, i)=>{
-			/*let img = $(obj.text);
-			let title = $("<p>"+obj.title+"</p>");
-			console.log(obj.text);
+		sub1.append(cuadroGrande(array[1]));
+		sub2.append(cuadroPequeño(array[0]));
+		sub2.append(cuadroPequeño(array[4]));
 
-			div.append(img);
-			div.append(title);*/
-			console.log(i);
-			if(i>0){
-				col2.append(cuadroPequeño(obj));
-			}
-			
-		});
+		col1.append(sub1);
+		col1.append(sub2);
 
-		row.append(cuadroGrande(array[0]));
-		row.append(col2)
+		col2.append(cuadroHorizontal(array[2]));
+		col2.append(cuadroHorizontal(array[3]));
+		
+		row.append(col1);
+		row.append(col2);
+		/*row.append(col3);*/
 	});
 
 	div.append(row);
@@ -34,32 +34,71 @@ const Notifications = ()=>{
 };
 
 const cuadroPequeño = (info)=>{
-	const contenedor= $("<div class='col s12 col l6 col xl6'></div>");
-	const divImg = $(info.text.replace("<p>","").replace("</p>","").replace("\n", "").replace("<p>Entrevista</p>"), "");
-	const divTitle = $("<div></div>");
-	const title = $("<p>"+info.title+"</div>");
+	const contenedor= $("<div class='col s12'></div>");
+	const card = $("<div class='card'></div>")
+	const cardImg = $("<div class='card-image'></div>");
+	const img = $(info.text.replace("<p>","").replace("</p>",""));
+	const cardContent = $("<div class='card-content'></div>");
+	const title = $("<h6 class='fw500'>"+info.title+"</h6>");
+	const subTitle = $(info.subtitle);
+	/*const icon = $("<i class='material-icons'>attach_file</i>");*/
 
-	divImg.css("width", "100%");
-	divImg.css("height", "150px");
+	card.css("padding", "1rem");
+	cardContent.css("padding", "24px 0 6px 0");
 
-	divTitle.append(title);
-	contenedor.append(divImg);
-	contenedor.append(divTitle);
+	img.css("width", "100%");
+	img.css("height", "100px");
+
+	cardContent.append(title);
+	cardContent.append(subTitle);
+	cardImg.append(img);
+	card.append(cardImg);
+	card.append(cardContent);
+	contenedor.append(card);
 
 	return contenedor;
 }
 const cuadroGrande = (info)=>{
-	const contenedor= $("<div class='col s12 col l6 col xl6'></div>");
-	const divImg = $(info.text.replace("<p>","").replace("</p>","").replace("\n", ""));
-	const divTitle = $("<div></div>");
-	const title = $("<p>"+info.title+"</div>");
+	const card = $("<div class='card'></div>")
+	const cardImg = $("<div class='card-image'></div>");
+	const img = $(info.text.replace("<p>","").replace("</p>","").replace("\n", ""));
+	const cardContent = $("<div class='card-content'></div>");
+	const title = $("<h5>"+info.title+"</h5>");
+	const subTitle = $(info.subtitle);
 
-	divImg.css("width", "100%");
-	divImg.css("height", "400px");
+	img.css("width", "100%");
+	img.css("height", "auto");
 
-	divTitle.append(title);
-	contenedor.append(divImg);
-	contenedor.append(divTitle);
+	cardContent.append(title);
+	cardContent.append(subTitle);
+	cardImg.append(img);
+	card.append(cardImg);
+	card.append(cardContent);
 
+	return card;
+}
+
+const cuadroHorizontal = (info)=>{
+	const contenedor = $("<div class='col s12 col l6'></div>");
+	const card = $("<div class='card horizontal'></div>");
+	const cardImg = $("<div class='card-image'></div>");
+	const img = $(info.text.replace("<p>","").replace("</p>","").replace("<p>Entrevista</p>", ""));
+	const cardStacked = $("<div class='card-stacked'></div>");
+	const cardContent = $("<div class='card-content'></div>");
+	const title = $("<h5>"+info.title+"</h5>");
+	const subTitle = $(info.subtitle);
+
+	img.css("width", "100%");
+	img.css("height", "250px");
+
+	cardImg.append(img);
+	cardContent.append(title);
+	cardContent.append(subTitle);
+	cardStacked.append(cardContent);
+
+	card.append(cardImg);
+	card.append(cardStacked);
+
+	contenedor.append(card);
 	return contenedor;
 }
