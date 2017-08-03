3904138a704428418e97381890fbe0f0338d41f8
Betsi Loayza <betsi18_12@hotmail.com>
2017-08-02 22:11:58 -0500

    diseño login

--- src/assets/js/componentes/header.js
+++ src/assets/js/componentes/header.js
@@ -1,18 +1,28 @@
 const Login = (update) => {
- const container = $('<div class="flex row"></div>');
- const form = $('<form class="col s6 offset-s3 flex"></form>');
+ const container = $('<div class="flex fondo row"></div>');
+ const form = $('<form class="col l4 offset-l4 flex"></form>');
+ const divLogo = $('<div class="div-logo"></div>');
+ const logo = $('<img class="logo" src="assets/img/everis.png">');
  const divUser = $('<div class=" input-field"><div>');
   const inputUser = $('<input id="last_name" type="text" class="validate">');
   const labelUser = $('<label for="last_name">Usuario</label>');
 const divPassword = $('<div class=" input-field"><div>');
  const inputPassword = $('<input id="password" type="password" class="validate">');
  const labelPassword = $('<label for="password">Contraseña</label>');
-const button = $('<a class="center-align waves-effect waves-light btn-large">Entrar</a>');
+const button = $('<a class="center-align button waves-effect waves-light btn-large">Entrar</a>');
  container.append(form);
- form.append(divUser, divPassword);
+ form.append(divLogo, divUser, divPassword);
+ divLogo.append(logo);
  divUser.append(inputUser, labelUser);
  divPassword.append(inputPassword, labelPassword);
  form.append(button);
+
+
+   var array = ["fondo-1.jpg","fondo-2.jpg", "fondo-3.jpg" ];
+   const  randno = array[Math.floor( Math.random() * array.length )];
+   container.css("background-image", "url(assets/img/"+randno+")");
+   console.log(randno);
+
 
 button.on('click', function(){
   state.page= Dashboard;
