ac299543fa8645f0276bc8e25ecc6ea06568edb5
Ana Durand <ana.dc1004@gmail.com>
2017-08-03 00:23:27 -0500

    resultado de busqueda

--- src/assets/js/filter.js
+++ src/assets/js/filter.js
@@ -17,20 +17,23 @@ const showItem = (item, container, divResult) => {
   const itemDiv = $('<div class="row"></div>');
   const p1 = $('<span class="item-place__title col s9">'+ item.Nombre +'   '+item.Apellido +'   '+item.Apellido2 +'</span>');
   const p2 = $('<span class="col s3">'+item.DatosEmpleado.EMPLOYEE_NUMBER+'</span>');
+
   const parent = $('<div></div>');
-  const name = $('<h5>'+ item.Nombre + '  '+ item.Apellido +'</h5> <br>');
-  const mail = $('<p> '+item.DatosEmpleado.EMAIL+'</p>');
-  const codigo = $('<p> '+item.DatosEmpleado.EMPLOYEE_NUMBER+'</p>');
-  // const mail = $('<p> '+item.DatosEmpleado.EMAIL+'</p>');
-  // const mail = $('<p> '+item.DatosEmpleado.EMAIL+'</p>');
+  const divImg = $('<div class="col m5 s12"><img src=""></div>');
+  const divInfo = $('<div class="col m7 s12"></div>');
+  const name = $('<h5>'+ item.Nombre + '  '+ item.Apellido +'   '+item.Apellido2 +'</h5> <br>');
+  const mail = $('<p><span class="strong"> Correo: </span>'+item.DatosEmpleado.EMAIL+'</p>');
+  const codigo = $('<p><span class="strong"> N° Empleado: </span>'+item.DatosEmpleado.EMPLOYEE_NUMBER+'</p>');
 
   itemDiv.append(p1);
   itemDiv.append(p2);
 
-  parent.append(name);
-  parent.append(mail);
-  parent.append(codigo);
 
+  divInfo.append(name);
+  divInfo.append(codigo);
+  divInfo.append(mail);
+  parent.append(divImg);
+  parent.append(divInfo);
   itemDiv.on("click", (e) => {
     e.preventDefault();
     container.empty();
